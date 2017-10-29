301cb50b6a8b024f8e1b78b9ff746e667471c0cc
Shiba <gtaffor@gmail.com>
2017-10-29 17:01:40 +0100

    Ajout de la fonction play et modification du main pour pouvoir jouer une partie

--- src/PlateauFousFous.java
+++ src/PlateauFousFous.java
@@ -5,10 +5,22 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class PlateauFousFous implements Partie1 {
 	
 	static final public int TAILLE = 8;
+	
+	//Boolean servant à savoir à quel joueur de jouer
+	private boolean joueurCourant = false;
+	
+	public boolean getJoueurCourant(){
+		return joueurCourant;
+	}
+	
+	public void changeJoueurCourant(){
+		joueurCourant = !joueurCourant;
+	}
 
 	private enum Player{
 			r("r"),b("b"),v("-");
@@ -129,8 +141,37 @@ public class PlateauFousFous implements Partie1 {
 	public void play(String move, String player) {
 		// TODO Auto-generated method stub
 		
+		int pLettre = changeLettreParChiffre(move.charAt(0));
+		int sLettre = changeLettreParChiffre(move.charAt(3));
+		
+		if(pLettre != -1 && sLettre != -1 && isNumeric(""+move.charAt(1)) && isNumeric(""+move.charAt(4))){
+			
+			//On déplace les pions
+			plateau[sLettre][Integer.parseInt(move.charAt(4) + "")] = plateau[pLettre][Integer.parseInt(move.charAt(1) + "")];
+			
+			//Fin du tour on change de joueur
+			changeJoueurCourant();
+		}
 	}
 
+	private int changeLettreParChiffre(char caract){
+		int i = -1;
+		
+		switch(caract){
+		case 'A' : i = 0; break;
+		case 'B' : i = 1; break;
+		case 'C' : i = 2; break;
+		case 'D' : i = 3; break;
+		case 'E' : i = 4; break;
+		case 'F' : i = 5; break;
+		case 'G' : i = 6; break;
+		case 'H' : i = 7; break;
+		default : i = -1; break;
+		}
+		
+		return i;
+	}
+	
 	@Override
 	public boolean finDePartie() {
 		// TODO Auto-generated method stub
@@ -168,11 +209,44 @@ public class PlateauFousFous implements Partie1 {
 	public static void main(String[] args) {
 		PlateauFousFous p = new PlateauFousFous();
 		
-		p.setFromFile("test.txt");
+		//Test de chargement à partir d'un fichier
+		//p.setFromFile("test.txt");
+		//Affichage de la map
+		//p.showMap();
+		//Ecriture dans un fichier
+		//p.saveToFile("out.txt");
 		
-		p.showMap();
+		Scanner sc = new Scanner(System.in); //Sert à lire les entrées
+		String str = null; //On écrit les entrées dans ce string
+		Player e = null; //Enumération de joueur
 		
-		p.saveToFile("out.txt");
+		System.out.println("Bonjour appuyé sur 1 si vous voulez charger un fichier, sinon sur 2 pour charger une partie vierge : ");
+		str = sc.nextLine();
+		if(isNumeric(str)){
+			switch(str) {
+				case "1" : System.out.println("Veuillez rentrer le nom du fichier :");
+						   str = sc.nextLine();
+						   p.setFromFile(str);
+						   break;
+				case "2" : System.out.println("Chargement d'un terrain classique");
+						   p.saveToFile("out.txt");
+						   break;
+			}
+			
+			while(!p.finDePartie()){
+				do
+				{
+					e = p.getJoueurCourant() ? Player.b : Player.r;
+					System.out.println("Joueur " + e + "quel cout voulez vous jouez :");
+					str = sc.nextLine();
+				}while(!p.estValide(str,e.toString()));
+				
+				//Finir le déplacement
+				p.play(str,e.toString());
+				
+			}
+			
+		}
 	}
 
 }
